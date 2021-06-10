2da7b55f886999f12c1ee4a81452462ad887f89a
Jacob Lepler <lepler.jacob@gmail.com>
2021-06-10 08:26:49 -0400

    updated footer

--- src/commons/footer/styledFooter.js
+++ src/commons/footer/styledFooter.js
@@ -5,65 +5,19 @@ export const StyledFooter = styles.footer`
     width: 100%
     max-width: 1024px;
     height: 50px;
-    background: #2e373d;
     background: #1d3356; // blue
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-around;
     border-top: 1px solid white;
-    
-    span {
-        color: whitesmoke;
-    }
 `;
 
-export const EachContact = styles.div`
+export const FooterContent = styles.div`
     display: flex;
-    flex-direction;
+    flex-direction: column;
+    justify-content: space-around;
     align-items: center;
-    justify-content: flex-start;
-`
-
-export const LeftSide = styles.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: flex-start;
-    height: 60%;
-
-    svg {
-        margin-right: 5px;
-        font-size: 1.5rem;
-        padding: 5px;
-        background: rgb(69, 76, 82);
-        border-radius: 50%;
-        color: whitesmoke;
-    }
-`;
-
-export const RightSide = styles.div`
-    height: 50%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: flex-start;
     color: #d6af63; // Gold
     font-size: 1rem;
-
-    a {
-        padding: 0;
-        margin: 0;
-    }
-
-    svg {
-        font-size: 2.5rem;
-        color: whitesmoke;
-        padding: 5px;
-        cursor: pointer;
-    }
-
-    span {
-        color: grey;
-    }
 `;
